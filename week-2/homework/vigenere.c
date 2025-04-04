9f20c577385f05cbd77cf5b93fbcf926af2c6209
Yunis003 <hi.yunisqulamov@gmail.com>
2025-04-04 20:39:06 +0400

    week-2 Vigenere Task #Final

--- week-2/homework/vigenere.c
+++ week-2/homework/vigenere.c
@@ -4,39 +4,61 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-string converter(string text, int key);
 int shift(char c);
+string vigenere(string plaintext, string keyword);
 
 int main(int argc, string argv[])
 {
-    int key;
-    if (argc >= 2)
+    if (argc != 2)
     {
-        key = shift(argv[1][0]);
-        printf("%i\n", key);
-        string plaintext = get_string("plaintext: ");
-        converter(plaintext, key);
-        printf("ciphertext: %s\n", plaintext);
-        return 0;
-    }
-    else
-    {
-        printf("Undefined input: please try again");
+        printf("İstifadə: ./vigenere açar\n");
         return 1;
     }
+
+    string keyword = argv[1];
+    for (int i = 0, n = strlen(keyword); i < n; i++)
+    {
+        if (!isalpha(keyword[i]))
+        {
+            printf("Açar yalnız hərflərdən ibarət olmalıdır.\n");
+            return 1;
+        }
+    }
+
+    string plaintext = get_string("plaintext: ");
+    string ciphertext = vigenere(plaintext, keyword);
+    printf("ciphertext: %s\n", ciphertext);
+    return 0;
 }
 
 int shift(char c)
 {
-    c = toupper(c);
-    return c;
+    if (isupper(c))
+    {
+        return c - 'A';
+    }
+    else if (islower(c))
+    {
+        return c - 'a';
+    }
+    return 0;
 }
 
-string converter(string text, int key)
+string vigenere(string plaintext, string keyword)
 {
-    for (int i = 0, n = strlen(text); i < n; i++)
+    int key_len = strlen(keyword);
+    string ciphertext = plaintext;
+    int key_index = 0;
+
+    for (int i = 0, n = strlen(plaintext); i < n; i++)
     {
-        text[i] = text[i] + (key - 65);
+        if (isalpha(plaintext[i]))
+        {
+            int key_shift = shift(keyword[key_index % key_len]);
+            char base = isupper(plaintext[i]) ? 'A' : 'a';
+            ciphertext[i] = ((plaintext[i] - base + key_shift) % 26) + base;
+            key_index++;
+        }
     }
-    return text;
+    return ciphertext;
 }
