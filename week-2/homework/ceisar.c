2dba5dac427ebfa35e1d9e49a7071de02152f821
Gulamov Yunis <139101778+Yunis003@users.noreply.github.com>
2025-04-08 23:30:15 +0400

    week-3 Lecture Materials #6

--- week-2/homework/ceisar.c
+++ week-2/homework/ceisar.c
@@ -11,9 +11,11 @@ int main(int argc, string argv[])
     int key;
     if (argc == 2)
     {
+        // Convert string to integer
         key = atoi(argv[1]);
         printf("%i\n", key);
         string plaintext = get_string("plaintext: ");
+        // Convert plaintext to caesar code
         caesarCode(plaintext, key);
         printf("ciphertext: %s\n", plaintext);
         return 0;
@@ -24,14 +26,17 @@ int main(int argc, string argv[])
     }
 }
 
+// Convert normal text to caesar code
 string caesarCode(string text, int key)
 {
     for (int i = 0, n = strlen(text); i < n; i++)
     {
+        // if letters is lowercase
         if (islower(text[i]))
         {
             text[i] = (char) (((text[i] - 'a' + key) % 26) + 'a');
         }
+        // if letters is uppercase
         else if (isupper(text[i]))
         {
             text[i] = (char) (((text[i] - 'A' + key) % 26) + 'A');
